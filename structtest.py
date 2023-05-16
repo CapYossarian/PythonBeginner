b24324487a67f6899a1300997da8595d86913472
CapYossarian <litovka.sergey@gmail.com>
2023-05-16 10:47:05 +0300

    PythonBeginner_20230516_1046

--- structtest.py
+++ structtest.py
@@ -1,3 +1,11 @@
-for i in range(5):
-    print(i)
-    i=0
+#=>0..4
+# for i in range(5):
+#     print(i)
+#     i=0
+
+#Mid
+a_list = [1, 2,3,4,5,6,7]
+mid= len(a_list)//2
+print(mid)
+print(a_list[:mid])
+print(a_list[mid:])
