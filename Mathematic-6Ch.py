6301664473accedead41b9413e59328f9797e09b
CapYossarian <litovka.sergey@gmail.com>
2023-05-19 08:49:11 +0300

    PythonBeginner_20230519_0848

--- Mathematic-6Ch.py
+++ Mathematic-6Ch.py
@@ -1,3 +1,4 @@
+import math
 def is_even(n):
     return not n & 1
 def fizzbuzz(n):
@@ -11,9 +12,25 @@ def fizzbuzz(n):
         else:
             print(i)
 
-fizzbuzz(10)
+
+def is_prime(n):
+#простые числа
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i == 0:
+            return False
+    return True
+
+def find_primes(n):
+    return [i for i in range(2,n+1) if(is_prime(i))]
+
+#Вывод всех простых чисел в диапозоне
+print(find_primes(100))
 
 
+#Делится ли на 3, 5, 3 и 5
+#fizzbuzz(10)
+
+# Четное?
 # for i in range(1,11):
  #    print(i,"even?",is_even(i))
 
