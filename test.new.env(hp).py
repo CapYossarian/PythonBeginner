9e6d73a6b29bc97f51d5f7930fea9922f81db6a0
CapYossarian <litovka.sergey@gmail.com>
2024-10-24 18:53:06 +0300

    1

---
+++ test.new.env(hp).py
@@ -0,0 +1,5 @@
+#20241024 настройка среды разработки на HP
+import datetime
+current_datetime = datetime.datetime.now()
+print("24 октября 24 года. Настраиваю среду разработки Pyton на HP")
+print("Сегодня проверяю ", current_datetime)
